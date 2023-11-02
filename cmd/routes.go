30e2f74b7c6ad5db0ada9d1fd91f6d029589888a
Benediktus Satriya <benediktus@students.amikom.ac.id>
2023-11-02 09:13:29 +0700

    simple crud

--- cmd/routes.go
+++ cmd/routes.go
@@ -6,7 +6,15 @@ import (
 )
 
 func setupRoutes(app *fiber.App) {
-	app.Get("/", handlers.ListFacts)
+	app.Get("/", handlers.Home)
+
+	app.Get("/fact", handlers.ListFacts)
 
 	app.Post("/fact", handlers.CreateFact)
+
+	app.Get("/fact/:id", handlers.DetailFact)
+
+	app.Put("/fact/:id", handlers.UpdateFact)
+
+	app.Delete("/fact/:id", handlers.DeleteFact)
 }
--- handlers/facts.go
+++ handlers/facts.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+func Home(c *fiber.Ctx) error {
+	sayHello := "Hello, World!"
+	return c.Status(200).JSON(fiber.Map{
+		"message": sayHello,
+	})
+}
+
 func ListFacts(c *fiber.Ctx) error {
 	facts := []models.Fact{}
 
@@ -16,13 +23,82 @@ func ListFacts(c *fiber.Ctx) error {
 
 func CreateFact(c *fiber.Ctx) error {
 	fact := new(models.Fact)
-	if err := c.BodyParser(fact); err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"message": err.Error(),
+
+	if err := c.BodyParser(&fact); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Bad Request. Invalid request body",
 		})
 	}
 
+	if fact.Question == "" || fact.Answer == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Question and answer cannot be empty",
+		})
+	}
 	database.DB.Db.Create(&fact)
 
 	return c.Status(201).JSON(fact)
+}
+
+func DetailFact(c *fiber.Ctx) error {
+	fact := models.Fact{}
+
+	id := c.Params("id")
+
+	//if not found return 404 message not found
+	err := database.DB.Db.First(&fact, id).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "Not Found",
+		})
+	}
+	return c.Status(200).JSON(fact)
+}
+
+func UpdateFact(c *fiber.Ctx) error {
+	fact := models.Fact{}
+
+	id := c.Params("id")
+
+	err := database.DB.Db.First(&fact, id).Error
+	if err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "Not Found",
+		})
+	}
+
+	if err := c.BodyParser(&fact); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Bad Request. Invalid request body",
+		})
+
+	}
+
+	if fact.Question == "" || fact.Answer == "" {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Question and answer cannot be empty",
+		})
+	}
+
+	database.DB.Db.Save(&fact)
+
+	return c.Status(200).JSON(fact)
+}
+
+func DeleteFact(c *fiber.Ctx) error {
+	fact := models.Fact{}
+	id := c.Params("id")
+
+	if err := database.DB.Db.First(&fact, id).Error; err != nil {
+		return c.Status(404).JSON(fiber.Map{
+			"message": "Not Found",
+		})
+	}
+
+	database.DB.Db.Delete(&fact)
+
+	return c.Status(200).JSON(fiber.Map{
+		"message": "Fact successfully deleted",
+	})
+
 }
